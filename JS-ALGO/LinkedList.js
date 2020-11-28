b84a1a70fb96e80b0f9c4419961ea01a8b1d9c64
CharingJC <junczhu@outlook.com>
2020-11-28 22:06:18 +0800

    Add LinkedList.js

--- JS-ALGO/LinkedList.js
+++ JS-ALGO/LinkedList.js
@@ -1,10 +1,87 @@
-export default class LinkedListNode {
-    constructor(value, next=null) {
-        this.value = value;
-        this.next = next;
+import LinkedListNode from './LinkedListNode';
+import Comparator from '../../utils/comparator/Comparator';
+
+export default class LinkedList {
+    constructor(comparatorFunction) {
+        this.head = null;
+
+        this.tail = null;
+
+        this.compare = new Comparator(comparatorFunction);
     }
 
-    toString(callback) {
-        return callback ? callback(this.value) : `${this.value}`;
+    prepend(value) {
+        const newNode = new LinkedListNode(value, this.head);
+        if(!this.head) {
+            this.head = newNode;
+        }
+
+        if(!this.tail) {
+            this.tail = newNode;
+        }
+
+        return this;
+    }
+
+    append(value) {
+        const newNode = new LinkedListNode(value)
+
+        if(!this.head) {
+            this.head = newNode;
+        }
+
+        if(this.tail) {
+            this.tail.next = newNode;
+        }
+        this.tail = newNode;
+
+        return this;
+    }
+
+    delete(value) {
+        if(!this.head) {
+            return null;
+        }
+
+        let deletedNode = null;
+        let previousNode = null;
+        let currentNode = this.head;
+
+        while(currentNode) {
+            if (this.compare.equal(currentNode.value, value)) {
+                previousNode.next = currentNode.next;
+                deletedNode = currentNode
+            } else {
+                previousNode = currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+
+        if(this.compare.equal(this.tail.value, value)) {
+            this.tail = previousNode;
+        }
+        return deletedNode;
+    }
+
+    find({value = undefined, callback = undefined}) {
+        if(!this.head) {
+            return null;
+        }
+
+        let currentNode = this.head;
+        let findNode = null;
+        while(currentNode) {
+            if(callback && callback(currentNode.value)) {
+                findNode = currentNode;
+            }
+
+            if(value !== undefined && this.compare.equal(currentNode.value,value)) {
+                findNode = currentNode;
+            }
+
+            currentNode = currentNode.newNode;
+        }
+
+        return findNode;
     }
 }
