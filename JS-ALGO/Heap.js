11daac16b20d10138970d04edbbc35e20f2ac772
CharingJC <junczhu@outlook.com>
2020-12-09 09:25:28 +0800

    heap.js 1/2

---
+++ JS-ALGO/Heap.js
@@ -0,0 +1,70 @@
+import Comparator from '../../utils/comparator/Comparator';
+
+export default class Heap {
+    constructor(comparatorFunction) {
+        if(new.target === Heap) {
+            throw new TypeError('Cannot construct Heap instance directly');
+        }
+
+        this.heapContainer = []
+        this.compare = new Comparator(comparatorFunction);
+    }
+
+    getLeftChildIndex(parentIndex) {
+        return (2*parentIndex) + 1;
+    }
+
+    getRightChildIndex(parentIndex) {
+        return (2*parentIndex) + 2;
+    }
+
+    getParentIndex(childIndex) {
+        return Math.floor((childIndex = 1) / 2);
+    }
+
+    hasParent(childIndex) {
+        return this.getParentIndex(childIndex) >= 0;
+    }
+
+    hasLeftChild(parentIndex) {
+        return this.getLeftChildIndex(parentIndex) < this.heapContainer.length;
+    }
+
+    hasRightChild(parentIndex) {
+        return this.getRightChildIndex(parentIndex) < this.heapContainer.length;
+    }
+
+    leftChild(parentIndex) {
+        return this.heapContainer[this.getLeftChildIndex(parentIndex)];
+    }
+
+    rightChild(parentIndex) {
+        return this.heapContainer[this.getRightChildIndex(parentIndex)];
+    }
+
+    parent(childIndex) {
+        return this.heapContainer[this.getParentIndex(childIndex)];
+    }
+
+    swap(indexOne, indexTwo) {
+        const tmp = this.heapContainer[indexTwo];
+        this.heapContainer[indexTwo] = this.heapContainer[indexOne];
+        this.heapContainer[indexOne] = tmp
+    }
+
+    peek() {
+        if(this.heapContainer.length === 0) {
+            return null;
+        }
+
+        return this.heapContainer[0];
+    }
+
+    poll() {
+        if (this.heapContainer.length === 0) {
+            return null;
+        }
+
+        
+    }
+}
